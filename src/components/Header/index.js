a7e4d1ad07570ae453f47251d47e3cdf7512ac1e
Minoru Saito <u.minor3110@gmail.com>
2018-05-27 03:39:16 +0900

    added title logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,12 +3,24 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
 
-export default function Header(props) {
+import logo from '../../images/jinro.svg'
+
+const styles = {
+  logo: {
+    width: 48,
+    height: 48
+  }
+}
+
+function Header(props) {
+  const { classes } = props
   return (
     <AppBar position="static" color="default">
       <Toolbar>
+        <img src={logo} alt="jinro chat" className={classes.logo} />
         <Button color="inherit" component={Link} to="/">
           <Typography variant="title" color="inherit">
             Jinro Chat
@@ -18,3 +30,5 @@ export default function Header(props) {
     </AppBar>
   )
 }
+
+export default withStyles(styles)(Header)
