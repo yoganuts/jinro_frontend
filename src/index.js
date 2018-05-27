1f14c23651107480f0eb8258918f054bac9dfa3a
Minoru Saito <u.minor3110@gmail.com>
2018-05-27 20:33:27 +0900

    apply custom theme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import createBrowserHistory from 'history/createBrowserHistory'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import blueGrey from '@material-ui/core/colors/blueGrey'
 
 import App from './App'
 import createStore from './createStore'
@@ -10,11 +12,22 @@ import createStore from './createStore'
 const history = createBrowserHistory()
 const store = createStore(history)
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blueGrey
+  },
+  typography: {
+    fontFamily: ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+  }
+})
+
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
