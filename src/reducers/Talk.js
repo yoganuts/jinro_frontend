1089f9275b98d80a6364bb8603a7b2d84d2c3055
Minoru Saito <u.minor3110@gmail.com>
2018-05-27 23:05:03 +0900

    [#6][bugfix] broadcast talk will be displayed in duplicate

--- src/reducers/Talk.js
+++ src/reducers/Talk.js
@@ -11,7 +11,10 @@ export default handleActions({
   RECEIVE_TALKS: (state, action) => ({
     talks: action.payload
   }),
-  RECEIVE_TALK: (state, action) => ({
-    talks: [action.payload, ...state.talks,]
-  })
+  RECEIVE_TALK: (state, action) => {
+    const exist = state.talks.some(talk => talk.id === action.payload.id)
+    return {
+      talks: exist ? state.talks : [action.payload, ...state.talks,]
+    }
+  }
 }, initialState)
