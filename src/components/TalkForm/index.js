a899be939606785b40909d6e5244117f5844d62b
Minoru Saito <u.minor3110@gmail.com>
2018-06-02 23:06:17 +0900

    remove unuse class

--- src/components/TalkForm/index.js
+++ src/components/TalkForm/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
 import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import SendIcon from '@material-ui/icons/Send'
