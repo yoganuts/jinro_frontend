c07bd6b88447ea6423a9503c8f5f80fc7b59e127
Minoru Saito <u.minor3110@gmail.com>
2018-06-03 17:55:31 +0900

    [#15]adjust search bar style

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchIcon from '@material-ui/icons/Search'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = {
   root: {
     display: 'flex',
     flexDirection: 'column',
+    paddingTop: 5,
+    paddingBottom: 3,
   },
   base: {
     display: 'flex',
     paddingLeft: 16,
     paddingRight: 16,
+  },
+  input: {
+    border: '1px solid #ccc',
+    borderRadius: 16,
   }
 }
 
@@ -24,6 +32,17 @@ function SearchBar(props) {
           fullWidth
           placeholder="検索"
           onChange={ (e) => props.onChange(e) }
+          InputProps={{
+            disableUnderline: true,
+            classes: {
+              root: classes.input,
+            },
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
         />
       </div>
     </div>
